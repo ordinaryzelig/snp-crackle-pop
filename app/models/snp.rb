657bd12369dc93f532cbc2b645e3f0ece657efcc
Jared Ning <jared@redningja.com>
2011-03-07 21:36:05 -0600

    DRYed XML parsing.

--- app/models/snp.rb
+++ app/models/snp.rb
@@ -1,60 +1,49 @@
 class Snp
 
   include Mongoid::Document
-  #include Mongoid::Timestamps # adds created_at and updated_at fields
 
-  field :rs_number, :type => Integer
-  field :chromosome, :type => Integer
-  field :gene, :type => String
-  field :accession, :type => Integer
-  field :organism, :type => String
-  field :function_class, :type => Integer
-  field :not_reference_assembly, :type => Boolean
-  field :heterozygosity, :type => Float
-  field :het_uncertainty, :type => Float
-  field :min_success_rate, :type => Float
-  field :max_success_rate, :type => Float
-  field :allele, :type => String
-  field :snp_class, :type => String
-  field :base_position, :type => Integer
+  field :rs_number,              type: Integer, xml: proc { |doc| doc.css('Rs').first['rsId'] }
+  field :chromosome,             type: Integer, xml: proc { |doc| doc.css('Rs Assembly Component').first['chromosome'] }
+  field :gene,                   type: String,  xml: proc { |doc| doc.css('Rs Assembly Component MapLoc FxnSet').first['symbol'] }
+  field :accession,              type: Integer  # Many accessions, grab all?
+  field :organism,               type: String#,  xml: proc { |doc| doc.css('SourceDatabase').first['organism'] }
+  field :function_class,         type: Integer
+  field :not_reference_assembly, type: Boolean, xml: proc { |doc| doc.css('Rs Assembly').first['reference'] }
+  field :heterozygosity,         type: Float,   xml: proc { |doc| doc.css('Rs Het').first['value'].to_f().round(3) }
+  field :het_uncertainty,        type: Float,   xml: proc { |doc| doc.css('Rs Het').first['stdError'].to_f().round(3) }
+  field :min_success_rate,       type: Float,   xml: proc { |doc| doc.css('Rs').first['validProbMin'].to_f() }
+  field :max_success_rate,       type: Float,   xml: proc { |doc| doc.css('Rs').first['validProbMax'].to_f() }
+  field :allele,                 type: String
+  field :snp_class,              type: String,  xml: proc { |doc| doc.css('Rs').first['snpClass'] }
+  field :base_position,          type: Integer
 
   # You can define indexes on documents using the index macro:
   # index :field <, :unique => true>
 
-  def self.fetch(rs_number)
-    response = Entrez.efetch('snp', {id: rs_number, retmode: 'xml'})
-    #... parse XML.
-    document = Nokogiri::XML(response.body)
-    rsId = document.root.css('Rs').first['rsId']
-    chromosome = document.root.css('Rs Assembly Component').first['chromosome']
-    gene = document.root.css('Rs Assembly Component MapLoc FxnSet').first['symbol']
-    #accession      Many accessions, grab all?
-    #organism = document.root.css('SourceDatabase').first['organism']
-    #function_class
-    not_reference_assembly = document.root.css('Rs Assembly').first['reference']
-    heterozygosity = document.root.css('Rs Het').first['value'].to_f().round(3)
-    het_uncertainty = document.root.css('Rs Het').first['stdError'].to_f().round(3)
-    min_success_rate = document.root.css('Rs').first['validProbMin'].to_f()
-    max_success_rate = document.root.css('Rs').first['validProbMax'].to_f()
-    #allele
-    snp_class = document.root.css('Rs').first['snpClass']
-    #base_position
-    snp = new rs_number: rsId, chromosome: chromosome, gene: gene,
-          not_reference_assembly: not_reference_assembly, snp_class: snp_class,
-          heterozygosity: heterozygosity, het_uncertainty: het_uncertainty,
-          min_success_rate: min_success_rate, max_success_rate: max_success_rate
-    #snp.save!
-    snp
-  end
-
-  def self.find_or_fetch(rs_number)
-    where(rs_number: rs_number).first || fetch!(rs_number)
-  end
-
   class << self
 
-    def search(rs_number)
-      find_or_fetch rs_number
+    def fetch(rs_number)
+      response = Entrez.efetch('snp', {id: rs_number, retmode: 'xml'})
+      attributes = parse(response.body)
+      snp = new(attributes)
+      #snp.save!
+      snp
+    end
+
+    def find_or_fetch(rs_number)
+      where(rs_number: rs_number).first || fetch!(rs_number)
+    end
+
+    private
+
+    # Given XML string, parse attributes based on xml procs defined for each field.
+    def parse(xml)
+      document = Nokogiri::XML(xml)
+      fields.inject({}) do |attributes, (field_name, field)|
+        xml_proc = field.options[:xml]
+        attributes[field_name] = xml_proc.call(document) if xml_proc
+        attributes
+      end
     end
 
   end
--- spec/models/snp_spec.rb
+++ spec/models/snp_spec.rb
@@ -2,58 +2,68 @@ require 'spec_helper'
 
 describe Snp do
 
+  # This is here just as a reminder to find an rs# that has all fields.
+  # It would help to test that it all works together.
+  # We can then also get rid of tests for individual fields.
+  it 'should fetch data from NCBI (see note in spec)'
+
   it 'should fetch data from NCBI' do
     rs = 121908378
     snp = Snp.fetch(rs)
     snp.rs_number.should eq(rs)
     attributes = {
       "rs_number" => 121908378,
-      "gene" => "FOXP2",
       "chromosome" => 7,
+      "gene" => "FOXP2",
+      #'accession' => '',
+      #'organism' => '',
+      #'function_class' => '',
       "not_reference_assembly" => true,
-      "snp_class" => "snp",
       "heterozygosity" => 0.0,
       "het_uncertainty" => 0.0,
       "min_success_rate" => 0.0,
       "max_success_rate" => 0.0,
+      #'allele' => '',
+      "snp_class" => "snp",
+      #'base_position' => '',
     }
     snp_attributes = snp.attributes
     snp_attributes.delete("_id")
     snp_attributes.should eq(attributes)
   end
 
-  it "should find gene name \"FOXP2\" for rsid# 121908378" do
+  it "should fetch gene" do
     rs = 121908378 # foxp2 rs no.
     snp = Snp.fetch(rs)
     snp.gene.should eq("FOXP2")
   end
 
-  it "should find organism name \"Agelaius phoeniceus\" for rsid# 119028036"# do
+  it "should fetch organism"# do
     #rs = 119028036 # red-winged blackbird rs no.
     #snp = Snp.fetch(rs)
     #snp.organism.should eq("Agelaius phoeniceus")
   #end
 
-  it "should find rsid#121908378 is a reference assembly" do
+  it "should fetch reference assembly" do
     rs = 121908378
     snp = Snp.fetch(rs)
     snp.not_reference_assembly.should eq(true)
   end
 
-  it "should find snp class \"snp\" for rsid#121908378" do
+  it "should fetch SNP class" do
     rs = 121908378
     snp = Snp.fetch(rs)
     snp.snp_class.should eq("snp")
   end
 
-  it "should find heterozygosity 0.045 +/- 0.143 for rsid#4884357" do
+  it "should fetch heterozygosity" do
     rs = 4884357
     snp = Snp.fetch(rs)
     snp.heterozygosity.should eq(0.045)
     snp.het_uncertainty.should eq(0.143)
   end
 
-  it "should find success rate 95% for rsid#1494555" do
+  it "should fetch success rate" do
     rs = 1494555
     snp = Snp.fetch(rs)
     snp.min_success_rate.should eq(95)

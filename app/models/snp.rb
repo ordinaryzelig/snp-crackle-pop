1ec22dd7a32e8a471dc2af7c561d1e06108588b9
Jared Ning <jared@redningja.com>
2011-05-25 15:40:05 -0500

    Implemented Snp#modification date and build.

--- app/models/snp.rb
+++ app/models/snp.rb
@@ -7,21 +7,23 @@ class Snp
 
   field :accession,          type: Integer  # Many accessions, grab all?
   embeds_many :alleles,                        options: {xml: :parse_alleles}, autosave: true
-  field :ancestral_allele,   type: String,  xml: proc { |doc| doc.css('Rs > Sequence').first['ancestralAllele'] }
+  field :ancestral_allele,   type: String,   xml: proc { |doc| doc.css('Rs > Sequence').first['ancestralAllele'] }
   field :base_position,      type: Integer
-  field :chromosome,         type: Integer, xml: proc { |doc| doc.css('Rs Assembly Component').first['chromosome'] }
+  field :chromosome,         type: Integer,  xml: proc { |doc| doc.css('Rs Assembly Component').first['chromosome'] }
   field :function_class,     type: Integer
-  field :gene_symbol,        type: Integer, xml: proc { |doc| doc.css('Rs Assembly Component MapLoc FxnSet').first['symbol'] }
-  field :het_uncertainty,    type: Float,   xml: proc { |doc| doc.css('Rs Het').first['stdError'].to_f.round(3) }
-  field :heterozygosity,     type: Float,   xml: proc { |doc| doc.css('Rs Het').first['value'].to_f.round(3) }
-  field :max_success_rate,   type: Float,   xml: proc { |doc| doc.css('Rs').first['validProbMax'] }
-  field :min_success_rate,   type: Float,   xml: proc { |doc| doc.css('Rs').first['validProbMin'] }
-  field :ncbi_gene_id,       type: Integer, xml: proc { |doc| doc.css('Rs Assembly Component MapLoc FxnSet').first['geneId'] }
-  field :ncbi_id,            type: Integer, xml: proc { |doc| doc.css('Rs').first['rsId'] }
-  field :ncbi_taxonomy_id,   type: Integer, xml: proc { |doc| doc.css('Rs').first['taxId']}
-  field :reference_assembly, type: Boolean, xml: proc { |doc| doc.css('Rs Assembly').any? { |assembly| assembly['reference'] == 'true'} }
-  field :rs_number,          type: Integer, xml: proc { |doc| doc.css('Rs').first['rsId'] }
-  field :snp_class,          type: String,  xml: proc { |doc| doc.css('Rs').first['snpClass'] }
+  field :gene_symbol,        type: Integer,  xml: proc { |doc| doc.css('Rs Assembly Component MapLoc FxnSet').first['symbol'] }
+  field :het_uncertainty,    type: Float,    xml: proc { |doc| doc.css('Rs Het').first['stdError'].to_f.round(3) }
+  field :heterozygosity,     type: Float,    xml: proc { |doc| doc.css('Rs Het').first['value'].to_f.round(3) }
+  field :max_success_rate,   type: Float,    xml: proc { |doc| doc.css('Rs').first['validProbMax'] }
+  field :min_success_rate,   type: Float,    xml: proc { |doc| doc.css('Rs').first['validProbMin'] }
+  field :modification_build, type: Integer,  xml: proc { |doc| doc.css('Rs Update').first['build'] }
+  field :modification_date,  type: Time,     xml: proc { |doc| doc.css('Rs Update').first['date'] }
+  field :ncbi_gene_id,       type: Integer,  xml: proc { |doc| doc.css('Rs Assembly Component MapLoc FxnSet').first['geneId'] }
+  field :ncbi_id,            type: Integer,  xml: proc { |doc| doc.css('Rs').first['rsId'] }
+  field :ncbi_taxonomy_id,   type: Integer,  xml: proc { |doc| doc.css('Rs').first['taxId']}
+  field :reference_assembly, type: Boolean,  xml: proc { |doc| doc.css('Rs Assembly').any? { |assembly| assembly['reference'] == 'true'} }
+  field :rs_number,          type: Integer,  xml: proc { |doc| doc.css('Rs').first['rsId'] }
+  field :snp_class,          type: String,   xml: proc { |doc| doc.css('Rs').first['snpClass'] }
   ncbi_timestamp_field
   belongs_to :gene
 

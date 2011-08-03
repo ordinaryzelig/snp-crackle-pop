7e43930372e5f4c3648b20c2e53b441e677aa72e
Jared Ning <jared@redningja.com>
2011-08-03 11:19:26 -0500

    Gene parses sequence range and length

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -6,15 +6,21 @@ class Gene
   split_xml_on { |doc| doc.css('Entrezgene') }
   set_ncbi_base_uri 'http://www.ncbi.nlm.nih.gov/gene/'
 
-  field :description,      type: String,  xml: proc { |node| node.css('Gene-ref_desc').first.content }
-  field :diseases,         type: Array,   xml: proc { |node| node.css('Entrezgene_comments > Gene-commentary > Gene-commentary_comment > Gene-commentary > Gene-commentary_type').select { |node| node['value'] == 'phenotype' }.map { |node| node.next.next.content } }
-  field :location,         type: String,  xml: proc { |node| node.css('Gene-ref_maploc').first.content }
-  field :mim,              type: Integer, xml: proc { |node| node.css('Entrezgene_unique-keys Dbtag_db').detect { |node| node.content == 'MIM' }.next.next.css('Object-id_id').first.content }
-  field :ncbi_id,          type: Integer, xml: proc { |node| node.css('Gene-track_geneid').first.content }
-  field :ncbi_taxonomy_id, type: Integer, xml: proc { |node| node.css('Org-ref_db Dbtag Dbtag_tag Object-id Object-id_id').first.content }
-  field :protein_name,     type: String,  xml: proc { |node| node.css('Prot-ref_name_E').first.content }
-  field :symbol,           type: String,  xml: proc { |node| node.css('Gene-ref_locus').first.content }
-  field :symbols_other,    type: Array,   xml: proc { |node| node.css('Gene-ref_syn Gene-ref_syn_E').map(&:content) }
+  field :description,            type: String,  xml: lambda { |node| node.css('Gene-ref_desc').first.content }
+  field :diseases,               type: Array,   xml: lambda { |node| node.css('Entrezgene_comments > Gene-commentary > Gene-commentary_comment > Gene-commentary > Gene-commentary_type').select { |node| node['value'] == 'phenotype' }.map { |node| node.next.next.content } }
+  field :location,               type: String,  xml: lambda { |node| node.css('Gene-ref_maploc').first.content }
+  field :mim,                    type: Integer, xml: lambda { |node| node.css('Entrezgene_unique-keys Dbtag_db').detect { |node| node.content == 'MIM' }.next.next.css('Object-id_id').first.content }
+  field :ncbi_id,                type: Integer, xml: lambda { |node| node.css('Gene-track_geneid').first.content }
+  field :ncbi_taxonomy_id,       type: Integer, xml: lambda { |node| node.css('Org-ref_db Dbtag Dbtag_tag Object-id Object-id_id').first.content }
+  field :protein_name,           type: String,  xml: lambda { |node| node.css('Prot-ref_name_E').first.content }
+  # Sequence data uses first matching occurrence in XML even though there may be multiples.
+  # Minimal testing shows "Genomic context" on NCBI site to match 1st occurrence.
+  field :sequence_accession,     type: String,  xml: lambda { |node| node.css('Entrezgene_locus Gene-commentary Gene-commentary_accession').first.content }
+  field :sequence_interval_from, type: Integer, xml: lambda { |node| node.css('Entrezgene_locus Gene-commentary Gene-commentary_seqs Seq-loc Seq-loc_int Seq-interval Seq-interval_from').first.content }
+  field :sequence_interval_to,   type: Integer, xml: lambda { |node| node.css('Entrezgene_locus Gene-commentary Gene-commentary_seqs Seq-loc Seq-loc_int Seq-interval Seq-interval_to').first.content }
+  field :sequence_version,       type: Integer, xml: lambda { |node| node.css('Entrezgene_locus Gene-commentary Gene-commentary_version').first.content }
+  field :symbol,                 type: String,  xml: lambda { |node| node.css('Gene-ref_locus').first.content }
+  field :symbols_other,          type: Array,   xml: lambda { |node| node.css('Gene-ref_syn Gene-ref_syn_E').map(&:content) }
   ncbi_timestamp_field
   alias_method :name, :symbol
 
@@ -35,6 +41,15 @@ class Gene
 
   def all_symbols
     [symbol] + symbols_other
+  end
+
+  def sequence_range
+    "#{sequence_accession}.#{sequence_version} (#{sequence_interval_from}..#{sequence_interval_to})"
+  end
+
+  def sequence_length
+    return nil unless sequence_interval_to.present? && sequence_interval_from.present?
+    sequence_interval_to - sequence_interval_from + 1
   end
 
   class << self
--- spec/models/gene_spec.rb
+++ spec/models/gene_spec.rb
@@ -17,24 +17,27 @@ describe Gene do
       @object = gene
     end
 
-    it_parses_attribute :accessions,       :pending
-    it_parses_attribute :description,     'ubiquinol-cytochrome c reductase complex chaperone'
-    it_parses_attribute :diseases,         ["A genome-wide association study in 19 633 Japanese subjects identified LHX3-QSOX2 and IGF1 as adult height loci.",
-                                            "Common variants in the GDF5-UQCC region are associated with variation in human height.",
-                                            "Genome-wide association analysis identifies 20 loci that influence adult height.",
-                                            "Identification of ten loci associated with height highlights new biological pathways in human growth.",
-                                            "Many sequence variants affecting diversity of adult human height.",
-                                            "Meta-analysis of genome-wide scans for human adult stature identifies novel Loci and associations with measures of skeletal frame size.",
-                                            "Stature quantitative trait locus 14",
-                                           ]
-    it_parses_attribute :length,           :pending
-    it_parses_attribute :location,         '20q11.22'
-    it_parses_attribute :ncbi_id,          55245
-    it_parses_attribute :ncbi_taxonomy_id, 9606
-    it_parses_attribute :mim,              611797
-    it_parses_attribute :protein_name,     'ubiquinol-cytochrome c reductase complex chaperone CBP3 homolog'
-    it_parses_attribute :symbol,           'UQCC'
-    it_parses_attribute :symbols_other,    ['BFZB', 'CBP3', 'C20orf44', 'MGC104353', 'MGC141902']
+    it_parses_attribute :accessions,             :pending
+    it_parses_attribute :description,           'ubiquinol-cytochrome c reductase complex chaperone'
+    it_parses_attribute :diseases,               ["A genome-wide association study in 19 633 Japanese subjects identified LHX3-QSOX2 and IGF1 as adult height loci.",
+                                                  "Common variants in the GDF5-UQCC region are associated with variation in human height.",
+                                                  "Genome-wide association analysis identifies 20 loci that influence adult height.",
+                                                  "Identification of ten loci associated with height highlights new biological pathways in human growth.",
+                                                  "Many sequence variants affecting diversity of adult human height.",
+                                                  "Meta-analysis of genome-wide scans for human adult stature identifies novel Loci and associations with measures of skeletal frame size.",
+                                                  "Stature quantitative trait locus 14",
+                                                 ]
+    it_parses_attribute :location,               '20q11.22'
+    it_parses_attribute :ncbi_id,                55245
+    it_parses_attribute :ncbi_taxonomy_id,       9606
+    it_parses_attribute :mim,                    611797
+    it_parses_attribute :protein_name,           'ubiquinol-cytochrome c reductase complex chaperone CBP3 homolog'
+    it_parses_attribute :sequence_accession,     'NC_000020'
+    it_parses_attribute :sequence_interval_from, 33890368
+    it_parses_attribute :sequence_interval_to,   33999944
+    it_parses_attribute :sequence_version,       10
+    it_parses_attribute :symbol,                 'UQCC'
+    it_parses_attribute :symbols_other,          ['BFZB', 'CBP3', 'C20orf44', 'MGC104353', 'MGC141902']
 
   end
 
@@ -80,5 +83,10 @@ describe Gene do
     stub_entrez_request_with_stubbed_response :EFetch, fixture_file_xml_content
     Gene.fetch(ids).map(&:ncbi_id).should == ids
   end
+
+   it 'calculates seequence length' do
+     gene = Gene.make_from_fixture_file
+     gene.sequence_length.should == 109_577
+   end
 
 end

34f4ed499948ef92cc23ecb4e8c3d7d194b66359
Jared Ning <jared@redningja.com>
2011-08-08 18:52:22 -0500

    Refactored snps/search to use unique id search if necessary.

--- app/controllers/SNPs.rb
+++ app/controllers/SNPs.rb
@@ -4,19 +4,19 @@ SnpCracklePop.controllers :snps do
     haml :'snps/download'
   end
 
-  # Instead of searching, just redirect to show.
+  # Perform unique id search.
   get :search do
-    rs_number = params[:q]
-    if rs_number
-      redirect url(:snps, :show, id: rs_number)
+    if params[:q]
+      rs_number = params[:q].match(/(?<number>\d+)/)[:number]
+      snp = Snp.find_all_by_unique_id_field_or_fetch_by_unique_id_field!([rs_number]).first
+      redirect url(:snps, :show, id: snp.rs_number)
     else
       haml :'snps/search'
     end
   end
 
   get :show, 'snps/:id' do
-    truncated_id = params[:id].match(/(?<number>\d+)/)[:number]
-    @snp = Snp.find_by_ncbi_id_or_fetch!(truncated_id)
+    @snp = Snp.find_by_ncbi_id_or_fetch!(params[:id])
     haml :'snps/show'
   end
 
